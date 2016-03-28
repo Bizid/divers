ebe26eef647f20bd7a31daeeaa66c40647a14557
Lassaad <bizid@yandex.ru>
2016-03-28 17:46:11 +0300

    Added files via upload

--- findPair.js
+++ findPair.js
@@ -25,14 +25,50 @@ function findPair2(input, desiredSum){
     for (var i = 0; i < input.length ; i++) {
         var firstElem = input[i];
         var secondElem = input.indexOf(desiredSum - firstElem);
+       // var secondElem = input.hasOwnProperty(desiredSum - firstElem);
+
         if (secondElem > -1) {
             resultPair[firstElem] = input[secondElem];
             input.splice(secondElem, 1);
         }
         j++;
     }
+    console.log(resultPair)
     return resultPair;
 }
+function findPair3(input, desiredSum){
+    var map = [];
+    var result= [];
+    var index;
+    for (var x = 0; x < input.length; x++) {
+        if (map[input[x]] != null) {
+            index = map[input[x]];
+            result.push({leftPart : input[index] ,rightPart : input[x]});
+        }
+        else {
+            map[desiredSum - input[x]] = x;
+        }
+    }
+    return result;
+}
 
-findPair2([3, 5, 7, 9, 4, 8, 25, 12, 4, 9, 16, 5], 28);
-findPair([3, 5, 7, 9, 4, 8, 25, 12, 4, 9, 16, 5], 28);
+
+function findPairSort(numbers, k) {
+    numbers.sort();
+    console.log(numbers);
+    var left = 0;
+    var right = numbers.length -1;
+    while(left < right){
+        var sum = numbers[left] + numbers[right];
+        if(sum == k){
+            console.log( numbers[left], numbers[right]);
+            left = left + 1;
+            right = right -1;
+        }else if(sum < k){
+            left = left +1;
+        }else if (sum > k) {
+            right = right -1;
+        }
+    }
+}
+
